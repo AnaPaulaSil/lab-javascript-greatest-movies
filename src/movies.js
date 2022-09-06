1fe9a7bf8732566fd440e2506df4f7e779058293
AnaPaulaSil <anapaulasilveira52783@gmail.com>
2022-09-06 10:50:23 -0300

    lab greatest

--- src/movies.js
+++ src/movies.js
@@ -1,25 +1,216 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
-
-// Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
-
-// Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
-
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
-
-// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
-
-// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
-
-// BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    const allDirectors = moviesArray.map((element) => {
+      return element.director;
+    });
+    return allDirectors;
+  }
+  
+  function getUniqueDirectors(getAllDirectors) {
+    let uniqueDirectors = [];
+    getAllDirectors.forEach((element) => {
+      if (!uniqueDirectors.includes(element)) {
+        uniqueDirectors.push(element);
+      }
+    });
+    return uniqueDirectors;
+  }
+  
+  // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+  function howManyMovies(moviesArray) {
+    const dramaSpielberg = moviesArray.filter((element) => {
+      return (
+        element.director === "Steven Spielberg" && element.genre.includes("Drama")
+      );
+    });
+    return dramaSpielberg.length;
+  }
+  
+  // Iteration 3: All scores average - Get the average of all scores with 2 decimals
+  function scoresAverage(moviesArray) {
+    const scoreAvg = moviesArray.reduce((acc, element) => {
+      return (acc + element.score) / moviesArray.length;
+    }, 0);
+    return +scoreAvg.toFixed(2);
+  }
+  
+  // Iteration 4: Drama movies - Get the average of Drama Movies
+  function dramaMoviesScore(moviesArray) {
+    const dramaMovies = moviesArray.filter((element) => {
+      return element.genre.includes("Drama");
+    });
+    const dramaAvg = dramaMovies.reduce((acc, element) => {
+      return (acc + element.score) / dramaMovies.length;
+    }, 0);
+    return +dramaAvg.toFixed(2);
+  }
+  
+  // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+  function orderByYear(moviesArray) {
+    const moviesByYear = [...moviesArray];
+  
+    moviesByYear.sort((a, b) => {
+      if (a.year === b.year) {
+        return ("" + a.title).localeCompare(b.title);
+      } else {
+        return a.year - b.year;
+      }
+    });
+  
+    return moviesByYear;
+  }
+  
+  // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+  function orderAlphabetically(moviesArray) {
+    let moviesByTitle = [...moviesArray];
+  
+    moviesByTitle.sort((a, b) => ("" + a.title).localeCompare(b.title));
+  
+    // PQ NAO POSSO FAZER TUDO ISSO EM UMA LINHA ???
+  
+    moviesByTitle = moviesByTitle.slice(0, 20).map((element) => {
+      return element.title;
+    });
+  
+    return moviesByTitle;
+  }
+  
+  // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+  function turnHoursToMinutes(moviesArray) {
+    let auxMovies = JSON.parse(JSON.stringify(moviesArray));
+  
+    // let auxMovies = [...moviesArray];
+    // TIVE QUE USAR PARSE E STRINGFY POIS COMO O METODO ACIMA COPIA O ARRAY POR REFERENCIA, QUANDO EU ADD OUTRAS COISAS NELE ELE ADICIONAVA NA ARRAY ORIGINAL
+  
+    let minutes = auxMovies.map((element) => {
+      if (
+        !isNaN(+element.duration.charAt(3)) &&
+        !isNaN(+element.duration.charAt(4))
+      ) {
+        return (
+          +element.duration.charAt(0) * 60 +
+          +element.duration.charAt(3) * 10 +
+          +element.duration.charAt(4)
+        );
+      } else if (
+        !isNaN(+element.duration.charAt(3)) &&
+        isNaN(+element.duration.charAt(4))
+      ) {
+        return +element.duration.charAt(0) * 60 + +element.duration.charAt(3);
+      } else {
+        return +element.duration.charAt(0) * 60;
+      }
+    });
+  
+    minutes.forEach((element, index) => {
+      auxMovies[index].duration = element;
+    });
+  
+    return auxMovies;
+  }
+  
+  console.log(movies);
+  console.log(turnHoursToMinutes(movies));
+  
+  // BONUS - Iteration 8: Best yearly score average - Best yearly score average
+  function bestYearAvg(moviesArray) {
+    if (!moviesArray.length) {
+      return null;
+    }
+  
+    let yearAvg = [...moviesArray];
+  
+    yearAvg.sort((a, b) => {
+      if (a.year === b.year) {
+        return b.score - a.score;
+      } else {
+        return a.year - b.year;
+      }
+    });
+  
+    let yearAndRate = { year: 0, rate: 0 };
+  
+    yearAvg.forEach((element, i) => {
+      if (yearAvg.length === 1) {
+        yearAndRate.year = element.year;
+        yearAndRate.rate = element.score;
+      } else {
+        if (
+          yearAvg[i + 1] !== undefined &&
+          element.year === yearAvg[i + 1].year
+        ) {
+          if (
+            yearAvg[i + 2] !== undefined &&
+            element.year === yearAvg[i + 2].year
+          ) {
+            if (
+              yearAvg[i + 3] !== undefined &&
+              element.year === yearAvg[i + 3].year
+            ) {
+              if (
+                yearAvg[i + 4] !== undefined &&
+                element.year === yearAvg[i + 4].year
+              ) {
+                if (
+                  yearAndRate.rate <
+                  (element.score +
+                    yearAvg[i + 1].score +
+                    yearAvg[i + 2].score +
+                    yearAvg[i + 3].score +
+                    yearAvg[i + 4].score) /
+                    5
+                ) {
+                  yearAndRate.year = element.year;
+                  yearAndRate.rate =
+                    (element.score +
+                      yearAvg[i + 1].score +
+                      yearAvg[i + 2].score +
+                      yearAvg[i + 3].score +
+                      yearAvg[i + 4].score) /
+                    5;
+                }
+              } else if (
+                yearAndRate.rate <
+                (element.score +
+                  yearAvg[i + 1].score +
+                  yearAvg[i + 2].score +
+                  yearAvg[i + 3].score) /
+                  4
+              ) {
+                yearAndRate.year = element.year;
+                yearAndRate.rate =
+                  (element.score +
+                    yearAvg[i + 1].score +
+                    yearAvg[i + 2].score +
+                    yearAvg[i + 3].score) /
+                  4;
+              }
+            } else if (
+              yearAndRate.rate <
+              (element.score + yearAvg[i + 1].score + yearAvg[i + 2].score) / 3
+            ) {
+              yearAndRate.year = element.year;
+              yearAndRate.rate =
+                (element.score + yearAvg[i + 1].score + yearAvg[i + 2].score) / 3;
+            }
+          } else if (
+            yearAndRate.rate <
+            (element.score + yearAvg[i + 1].score) / 2
+          ) {
+            yearAndRate.year = element.year;
+            yearAndRate.rate = (element.score + yearAvg[i + 1].score) / 2;
+          }
+        } else if (yearAndRate.rate < element.score) {
+          yearAndRate.year = element.year;
+          yearAndRate.rate = element.score;
+        }
+      }
+    });
+  
+    return `The best year was ${
+      yearAndRate.year
+    } with an average score of ${+yearAndRate.rate.toFixed(2)}`;
+  }
